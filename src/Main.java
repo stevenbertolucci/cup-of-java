b1aed52baa99abfbe78ca6a0c3e7b5d67c21b156
stevenbertolucci <steven.bertolucci@yahoo.com>
2023-11-19 10:40:09 -0800

    Added method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    // Method multiply array that multiplies the array with whatever value the user chose
+    static void multiplyArray(int[] args, int value) {
+        System.out.print("Under construction");
+    }
+
     public static void main(String[] args) {
 
         int[] array = {5, 3, 7, 1, 2, 4, 3, 2, 1};
+        int count = 0;
         System.out.println("Below is the array we have right now:\n" + Arrays.toString(array));
 
         // Manual Sort
@@ -34,5 +41,31 @@ public class Main {
         Arrays.sort(array2);
         System.out.println("Sorted array: \n" + Arrays.toString(array2));
 
+        // Create a scanner object
+        Scanner scannerObject = new Scanner(System.in);
+        System.out.print("\nDo you want to multiply the array? [Y/N] ");
+
+        while (true) {
+            String choice = scannerObject.nextLine();
+            // Check if user enters Y or N
+            if (choice.toUpperCase().equals("Y")) {
+                System.out.print("Enter a value: ");
+                int value = scannerObject.nextInt();
+                multiplyArray(array, value);
+                break;
+            } else if (choice.toUpperCase().equals("N")) {
+                System.out.print("Okey dokey.\n");
+                break;
+            } else {
+                count++;
+                // If user enters a wrong input every 3 times, display this message
+                if (count % 3 == 0) {
+                    System.out.print("All you have to do is enter the letter 'y' or 'n'. Try again: ");
+                } else {
+                    // Humorous quote from 'The Office' sitcom
+                    System.out.print("I SAID YES (Y) OR NO (N). DID I STUTTER? ");
+                }
+            }
+        }
     }
 }
